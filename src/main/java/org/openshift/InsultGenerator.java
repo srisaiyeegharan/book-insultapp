5975f73ad25e3e279104f4278137a5f469935ae1
Srisaiyeegharan Kidnapillai <100680347@student.swin.edu.au>
2018-01-17 14:02:08 +1100

    Modified code for connecting database

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -14,7 +14,7 @@ public class InsultGenerator {
 		databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
 
 		String username = System.getenv("POSTGRESQL_USER");
-		String password = System.getenv("PGPASSWORD");
+		String password = System.getenv("POSTGRESQL_USER_PASSWORD");
 		try {
 			Connection connection = DriverManager.getConnection(databaseURL,username,password);
 			if (connection != null)
